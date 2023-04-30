f1c920a149fcdfd0f8d8af4489aa1a73bbf987c0
Rodrigo Moreira <rodrigomoreiradasilva.of@gmail.com>
2023-04-30 09:39:13 -0300

    :sparkles: feat: Develops custom scrollbar

--- frontend/src/pages/app/home/index.tsx
+++ frontend/src/pages/app/home/index.tsx
@@ -15,12 +15,12 @@ const Home = () => {
         <main className="px-4 md:px-12 lg:px-16 pt-24 h-full w-full min-w-full lg:gap-12 lg:grid grid-cols-[2fr_minmax(340px,1fr)]">
           <section className="lg:col-start-1 lg:col-end-2">
           </section>
-          <aside className="h-full flex flex-col gap-7 lg:col-start-2 lg:col-end-3" aria-labelledby="recentTransactions_title">
+          <aside className="h-full flex flex-col gap-7 lg:col-start-2 lg:col-end-3 max-h-full overflow-hidden" aria-labelledby="recentTransactions_title">
             <header className="w-full flex items-center justify-between">
               <h2 id='recentTransactions_title' className="text-xl font-semibold text-typography-light/80">Últimas transações</h2>
               <Redirect path="/transactions" />
             </header>
-            <ul className="w-full flex flex-col gap-5">
+            <ul className="w-full flex flex-col gap-5 h-full max-h-full overflow-y-auto pb-6 lg:pb-11 custom-scrollbar">
               {
                 recentTransactions.map(transaction => <li key={transaction.id}><TransactionsCard data={transaction} /></li>)
               }
