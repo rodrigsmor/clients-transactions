e34685bd6323ed3bf986070d13ac387234a19991
Rodrigo Moreira <rodrigomoreiradasilva.of@gmail.com>
2023-05-01 08:45:04 -0300

    :bug: fix: Implements Error filter and improve error messages

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
-import { APP_GUARD } from '@nestjs/core';
+import { APP_GUARD, APP_PIPE } from '@nestjs/core';
 import { AccessTokenGuard } from './common/guards';
 import { CustomerController } from './api/customer/customer.controller';
 import { CustomerService } from './api/customer/customer.service';
@@ -10,10 +10,15 @@ import { ProductController } from './api/product/product.controller';
 import { ProductService } from './api/product/product.service';
 import { TransactionsController } from './api/transactions/transactions.controller';
 import { TransactionsService } from './api/transactions/transactions.service';
+import { CustomValidationPipe } from './utils/filters/custom.validation.pipe';
 
 @Module({
   imports: [AuthModule, PrismaModule, PrismaClient],
   providers: [
+    {
+      provide: APP_PIPE,
+      useClass: CustomValidationPipe,
+    },
     {
       provide: APP_GUARD,
       useClass: AccessTokenGuard,
---
+++ backend/src/utils/filters/custom.validation.pipe.ts
@@ -0,0 +1,31 @@
+import {
+  ArgumentMetadata,
+  BadRequestException,
+  Injectable,
+  PipeTransform,
+} from '@nestjs/common';
+import { plainToClass } from 'class-transformer';
+import { validate } from 'class-validator';
+
+@Injectable()
+export class CustomValidationPipe implements PipeTransform<any> {
+  async transform(value: any, metadata: ArgumentMetadata) {
+    const { metatype } = metadata;
+    if (!metatype || !this.toValidate(metatype)) return value;
+
+    const object = plainToClass(metatype, value);
+    const errors = await validate(object);
+
+    if (errors.length > 0) {
+      const messages = errors.map((error) => Object.values(error.constraints));
+      throw new BadRequestException(`Erro ao validar - ${messages}`);
+    }
+
+    return value;
+  }
+
+  private toValidate(metatype: any): boolean {
+    const types = [String, Boolean, Number, Array, Object];
+    return !types.includes(metatype);
+  }
+}
