63d76396196462463ead3f02107ca70bbf063d9d
Rodrigo Moreira <rodrigomoreiradasilva.of@gmail.com>
2023-05-06 14:29:40 -0300

    :construction: docs(backend): Configurate the Swagger UI in backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { GlobalExecptionFilter } from './utils/filters/global.exception.filter';
 import { json, urlencoded } from 'express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -13,6 +14,16 @@ async function bootstrap() {
   app.use(json({ limit: '100mb' }));
   app.use(urlencoded({ limit: '100mb', extended: true }));
   app.useGlobalFilters(new GlobalExecptionFilter());
+
+  const config = new DocumentBuilder()
+    .setTitle('API documentation')
+    .setDescription('API of a transactions manager.')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
